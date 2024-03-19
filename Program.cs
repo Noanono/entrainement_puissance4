568fb317af2558aed0deb7b36447d8861e4af7cf
Noah Soler <79003523+Noanono@users.noreply.github.com>
2024-03-19 07:56:53 +0100

    ajout pions explo diag et enclume plus meilleure gestion pions speciaux reste a verifier la gravite

--- Program.cs
+++ Program.cs
@@ -4,19 +4,12 @@ using System;
 Console.WriteLine("Entrez la taille du plateau :");
 var taille = int.Parse(Console.ReadLine());
 var plateau = new string[taille, taille];
-for (int i = 0; i < taille; i++)
-{
-    for (int j = 0; j < taille; j++)
-    {
-        plateau[i, j] = null;
-    }
-}
 
 Console.WriteLine("Entrez le nombre de joueurs :");
 var nbJoueurs = int.Parse(Console.ReadLine());
 var joueurs = new string[nbJoueurs, 2];
 var joueurs_c = new string[nbJoueurs];
-var joueurs_e = new int[nbJoueurs];
+var joueurs_e = new int[nbJoueurs, 4];
 for (int i = 0; i < nbJoueurs; i++)
 {
     Console.WriteLine($"Entrez le nom du joueur {i + 1} :");
@@ -56,26 +49,63 @@ for (int i = 0; i < nbJoueurs; i++)
     joueurs_c[i] = joueurs[i, 1];
 }
 
-for (int i = 0; i < nbJoueurs; i++)
-{
-    joueurs_e[i] = 3;
-}
 
 Debut_jeu:
+// Initialisation du plateau
+for (int i = 0; i < taille; i++)
+{
+    for (int j = 0; j < taille; j++)
+    {
+        plateau[i, j] = null;
+    }
+}
+
+// Initialisation des pions spéciaux
+for (int i = 0; i < nbJoueurs; i++)
+{
+    for (int j = 0; j < 4; j++)
+    {
+        joueurs_e[i, j] = 3;
+    }
+}
+
+// Début du jeu
 var joueurActuel = 0;
 var nbCases = taille * taille;
 var casesJouees = 0;
+
 while (true)
 {
-    var explo = "non";
     Console.WriteLine($"\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\nC'est au tour de {joueurs[joueurActuel, 0]} de jouer.");
     Afficher_plateau();
-    if (joueurs_e[joueurActuel] > 0)
+
+    // Choix pion spéciaux
+    var use = new string[4] { "non", "non", "non", "non" };
+
+    for (int i = 0; i < 4; i++)
     {
-        Console.WriteLine($"Il vous reste {joueurs_e[joueurActuel]} pions explosifs.");
-        Console.WriteLine("Voulez vous utiliser un piont eplosif (oui/non) :");
-        explo = Console.ReadLine();
+        if (joueurs_e[joueurActuel, i] > 0)
+        {
+            var type = i switch
+            {
+                0 => "pion explosif en forme de croix",
+                1 => "pion explosif en forme diagonale",
+                2 => "pion enclume",
+                3 => "pion bloquant",
+                _ => "pion classique",
+            };
+            Console.WriteLine($"Il vous reste {joueurs_e[joueurActuel, i]} {type}.");
+            Console.WriteLine($"Voulez vous utiliser un {type} (oui/non) :");
+            use[i] = Console.ReadLine();
+            if (use[i] == "oui")
+            {
+                break;
+            }
+        }
     }
+
+    // Fin choix pion spéciaux
+
     Console.WriteLine("Entrez la colonne de la case à jouer :");
     mauvaise_colonne:
     var colonne = int.Parse(Console.ReadLine()) - 1;
@@ -88,19 +118,25 @@ while (true)
     {
         if (plateau[ligne, colonne] == null)
         {
-            if (explo == "oui")
+            //gestion du pion explosif en forme de croix
+            if (use[0] == "oui")
             {
                 for (int i = -1; i <= 1; i++)
                 {
-                    for (int j = -1; j <= 1; j++)
+                    if ((ligne + i >= 0 && ligne + i <= taille - 1))
                     {
-                        if ((ligne + i >= 0 && ligne + i <= taille - 1) && (colonne + j >= 0 && colonne + j <= taille - 1) && plateau[ligne + i, colonne + j] != null)
-                        {
-                            plateau[ligne + i, colonne + j] = null;
-                        }
+                        plateau[ligne + i, colonne] = null;
+                    }
+                    
+                }
+                for (int j = -1; j <= 1; j++)
+                {
+                    if ((colonne + j >= 0 && colonne + j <= taille - 1))
+                    {
+                        plateau[ligne, colonne + j] = null;
                     }
                 }
-                joueurs_e[joueurActuel]--;
+                joueurs_e[joueurActuel, 0]--;
                 if (ligne + 1 < taille)
                 {
                     plateau[ligne + 1, colonne] = joueurs[joueurActuel, 1];
@@ -108,6 +144,37 @@ while (true)
                     break;
                 }
             }
+            //fin explo_croix
+
+            //gestion du pion explosif en forme diagonale
+            if (use[1] == "oui")
+            {
+                for (int i = -1; i <= 1; i++)
+                {
+                    if ((ligne + i >= 0 && ligne + i <= taille - 1) && (colonne + i >= 0 && colonne + i <= taille - 1) && (colonne - i >= 0 && colonne - i <= taille - 1))
+                    {
+                        plateau[ligne + i, colonne + i] = null;
+                        plateau[ligne + i, colonne - i] = null;
+                    }
+                }
+                joueurs_e[joueurActuel, 1]--;
+            }
+            //fin explo_diag
+
+            //gestion du pion enclume
+            if (use[2] == "oui")
+            {
+                for (int i = taille -1; i <= ligne; i--)
+                {
+                    plateau[i, colonne] = null;
+                }
+                joueurs_e[joueurActuel, 2]--;
+                plateau[taille - 1, colonne] = joueurs[joueurActuel, 1];
+                casesJouees++;
+                break;
+            }
+            //fin enclume
+
             plateau[ligne, colonne] = joueurs[joueurActuel, 1];
             casesJouees++;
             break;
